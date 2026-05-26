ce21a8596b64130499416f4ba85a17f07089cced
Murilo <delaramartinsmurilo@gmail.com>
2026-05-26 09:18:55 -0300

    Add files via upload

---
+++ zap zap/src/script.js
@@ -0,0 +1,9 @@
+function zap(){
+    const nome = document.getElementById('nome').value 
+    const email = document.getElementById('email').value
+    const phone = document.getElementById('phone').value
+
+    const mensagem = `Gostaria de entrar em contato!\n\nNome: ${nome}\nE-mail: ${email}\nTelefone: ${phone}`
+    const msg = encodeURIComponent(mensagem)
+    window.open(`https://wa.me/55dd********?text=${msg}`)
+}
